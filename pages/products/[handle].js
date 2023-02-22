494fcea83ab8c0ae723470ce52d5402a8390a1ba
Ian Greenfield <ian.greenfield@heydayskincare.com>
2023-02-22 15:49:30 -0600

    add builder.io page building

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -124,6 +124,22 @@ export default function ProductPage({ product }) {
             </div>
           </Container>
         </div>
+
+        {/* How We Use It */}
+        <div className='bg-[#f8f4f0] py-10'>
+          <Container>
+            <div className='border-b border-[#c2c6c9]'>
+              <div>
+                <span>How We Use It</span>
+              </div>
+              <div className='flex flex-col gap-3 py-8'>
+                <h3>Morning And Night</h3>
+                <p>Apply a few drops to clean, toned skin, focusing on areas that you feel need a bit of extra attention.</p>
+              </div>
+            </div>
+          </Container>
+        </div>
+
         {/* Treatment Room Tip */}
         <div className='bg-[#333f48] text-white py-10'>
           <Container>
